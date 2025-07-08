39fc617efab88d3d17fb4a4ed978b0af06086be2
Reset2412 <joshuapaz24@gmail.com>
2025-07-08 00:31:02 -0600

    Modificaciones realizadas:

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ app.secret_key = 'JoshuaPaz' # Clave secreta para sesiones y mensajes flash
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'database', 'creditos.db')}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
